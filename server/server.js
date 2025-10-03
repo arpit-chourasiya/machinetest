0e511f37d2f73c02bcd861d6842bb65f13e1db9b
Arpit <arpitchourasiya98@gamil.com>
2025-10-03 20:44:19 +0530

    fix the client and server package versions

--- server/server.js
+++ server/server.js
@@ -1,20 +1,33 @@
 /** @format */
 
 const express = require("express");
-require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-const connectDb = require("./config/dbConfig");
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
-connectDb();
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Server is running!" });
+  res.json({ message: "MERN Auth API is running!" });
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
